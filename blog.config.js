8a3bdf14392baf9dbf2c418ef93b91d048478224
Scu-LinWang <86878675+Scu-LinWang@users.noreply.github.com>
2023-04-12 21:05:09 +0800

    Update blog.config.js
    简单的修改

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Mr.Lin', // 您的昵称 
-  BIO: process.env.NEXT_PUBLIC_BIO || '不愿成为码农的码农🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '不愿做码农的码农🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
